ab3174fb87854aa16dd3a977db416103ab794e98
Abdigal <abdigal.cb@gmail.com>
2022-05-22 20:39:47 -0500

    Keep ratio v1

--- Preprocesamiento/dataLoader.py
+++ Preprocesamiento/dataLoader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import cv2
+import numpy as np
 import torch
 from torch.utils.data import DataLoader, Dataset
 import matplotlib.pyplot as plt
@@ -26,16 +27,31 @@ class belugaDataset(Dataset):
         x_0, y_0, _ = img.shape
 
         if x_0 >y_0:
+            x_0, y_0 = y_0, x_0
             img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
-        img = cv2.resize(img, self.size)
-        if self.keep_ratio:
-            r_y, r_x = self.size[0], self.size[1] 
-            pass
         
+        if self.keep_ratio:
+            r_y, r_x = self.size[0]/y_0, self.size[1]/x_0
+            r = min(r_y, r_x)
+            img = cv2.resize(img, (int(y_0*r), int(x_0*r)))
+            x_1, y_1, _ = img.shape
+            bg_img = np.zeros((self.size[1],self.size[0],3), dtype=np.uint8)
+            m_x = int((self.size[0]-y_1)/2)
+            m_y = int((self.size[1]-x_1)/2)
+            #print(m_x, m_y)
+            bg_img[m_y:m_y+x_1, m_x:m_x+y_1, :] = img
+            img = bg_img
+
+        else:  
+            img = cv2.resize(img, self.size)
         return {'x':img, 'date':date, 'y':y, 'vp':viewpoint} 
 
 
 if __name__ == "__main__":
-    ds = belugaDataset(csv_file=os.path.join(os.pardir, 'metadata.csv'), im_folder=os.path.join(os.pardir, 'images'))
-    print(ds[0])
+    ds = belugaDataset(csv_file=os.path.join(os.pardir, 'metadata.csv'), im_folder=os.path.join(os.pardir, 'images'), keep_ratio=True)
+    i = 3
+    print(ds[i])
+    print((ds[i]['x']).shape)
+    plt.imshow(ds[i]['x'])
+    plt.show()
